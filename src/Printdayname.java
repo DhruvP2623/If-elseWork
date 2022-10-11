013bfc28632ce8dce902f2a9dcc0bae4738e0004
Dhruv Patel <dhruvp6400@gmail.com>
2022-10-11 22:33:13 +0100

    Printdayname Commit

---
+++ src/Printdayname.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Printdayname {
+
+    static int number;
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);//Scanner object
+        System.out.println("Enter number: ");
+       int num = scanner.nextInt();// declared variable
+// Using switch
+        switch (num) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("No Day found");
+
+
+        }
+    }
+}
